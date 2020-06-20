1f6a1e6985a5d55f027cef816423b5c2fd32b889
Júlio César <mendes_jc@outlook.com>
2020-06-20 09:34:31 -0300

    Added all

---
+++ precache-manifest.62f42433f482aaa4c69421bc64176fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6e17f16881eb9fec5ae4cdf5408ae4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8850402fc3b4428473e5",
+    "url": "/static/js/2.0e7cdb64.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.0e7cdb64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417492d65734a6be41a1",
+    "url": "/static/js/main.00da8c2b.chunk.js"
+  },
+  {
+    "revision": "8f2579ff55bd5aa1997a",
+    "url": "/static/js/runtime-main.87831706.js"
+  },
+  {
+    "revision": "1c7029ef934d22ff47f05da90168baa3",
+    "url": "/static/media/about.1c7029ef.md"
+  }
+]);
---
+++ static/media/about.1c7029ef.md
@@ -0,0 +1,8 @@
+Hello!!\
+
+As you can see up here, my name is Júlio.
+Being more especific, Júlio César Mendes.
+
+```
+test
+```
